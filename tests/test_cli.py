8a963f2725f2f020d95beb9731e94f35155508ad
Radoslaw Kiczko <rkicz@softserveinc.com>
2020-12-30 09:26:23 +0100

    30882 Feat: Move gordo cli code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,13 @@
+import json
+import tempfile
+
 import pytest
 from click.testing import CliRunner
 
+from gordo_dataset.data_provider import providers
+
 from gordo.client.cli.client import gordo_client
+from gordo.client.cli.custom_types import DataProviderParam
 
 
 @pytest.fixture
@@ -19,3 +25,28 @@ def test_version(runner):
 
     assert result.exit_code == 0
     assert result.output.startswith("gordo-client, version")
+
+
+
+@pytest.mark.parametrize(
+    "config",
+    (
+        '{"type": "RandomDataProvider", "max_size": 200}',
+        '{"type": "InfluxDataProvider", "measurement": "value"}',
+    ),
+)
+def test_data_provider_click_param(config, sensors_str):
+    """
+    Test click custom param to load a provider from a string config representation
+    """
+    expected_provider_type = json.loads(config)["type"]
+    provider = DataProviderParam()(config)
+    assert isinstance(provider, getattr(providers, expected_provider_type))
+
+    # Should also be able to take a file path with the json
+    with tempfile.NamedTemporaryFile(mode="w") as config_file:
+        json.dump(json.loads(config), config_file)
+        config_file.flush()
+
+        provider = DataProviderParam()(config_file.name)
+        assert isinstance(provider, getattr(providers, expected_provider_type))
