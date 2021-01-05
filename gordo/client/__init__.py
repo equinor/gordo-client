2c8e34d91f239c546720dad5506b2e49cd33609b
Radoslaw Kiczko <rkicz@softserveinc.com>
2021-01-05 10:15:06 +0100

    Add gorodo.client __version__

--- gordo/client/__init__.py
+++ gordo/client/__init__.py
@@ -1,7 +1,11 @@
 from pkgutil import extend_path
+from pkg_resources import get_distribution
 
 from gordo.client.client import Client
 from gordo.client.utils import influx_client_from_uri
+
+
+__version__ = get_distribution('gordo.client').version
 
 # Denote a package as a namespace package.
 # https://www.python.org/dev/peps/pep-0420/#namespace-packages-today
